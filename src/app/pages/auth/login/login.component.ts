5e52da87852febbda42ee582d4697794932eb7e7
Aditya Harsh <52870951+adityaharsh2001@users.noreply.github.com>
2021-02-08 19:50:18 +0000

    Update login.component.ts

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from "../auth.service";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./styles.css']
 })
 export class LoginComponent {
 
